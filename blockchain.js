9c0331d3203f710c04ed78037271a05b2ac40cfb
Marcus <marcus.kalmar@gmail.com>
2019-11-28 14:01:12 +0100

    Fix hash issue

--- blockchain.js
+++ blockchain.js
@@ -1,49 +1,58 @@
-const sha256 = require('./node_modules/js-sha256')
+const sha256 = require('js-sha256')
 const Block = require('./block')
 
 class Blockchain {
-    constructor (genesisBlock) {
-        this.blocks = []
-        this.addBlock(genesisBlock)
+
+  constructor(genesisBlock) {
+
+    this.blocks = []
+    this.addBlock(genesisBlock)
+  }
+
+  addBlock(block) {
+
+    if (this.blocks.length == 0) {
+      block.previousHash = "0000000000000000"
+      block.hash = this.generateHash(block)
     }
 
-    addBlock(block) {
-        if (this.blocks.length == 0) {
-            block.previousHash = '00000000000000'
-            block.hash = this.generateHash(block)
-        }
+    this.blocks.push(block)
 
-        this.blocks.push(block)
-    }
+  }
 
-    getNextBlock(transactions) {
-        const block = new Block()
+  getNextBlock(transactions) {
 
-        transactions.forEach((transaction) => {
-            block.addTransaction(transaction)
-        })
+      const block = new Block()
 
-        const previousBlock = this.getPreviousBlock()
-        block.index = this.blocks.length
-        block.previousHash = previousBlock.hash
-        block.hash = this.generateHash(block)
-    }
+      transactions.forEach((transaction) => {
+        block.addTransaction(transaction)
+      })
 
-    getPreviousBlock() {
-        return this.blocks[this.block.length - 1]
-    }
+      let previousBlock = this.getPreviousBlock()
+      block.index = this.blocks.length
+      block.previousHash = previousBlock.hash
+      block.hash = this.generateHash(block)
 
-    generateHash(block) {
-        const hash = sha256(block.key)
+      return block
+  }
 
-        while(!hash.startsWith('00000')) {
-            block.nonce += 1
-            hash = sha256(block.key)
-            console.log(hash)
-        }
+  getPreviousBlock() {
+    return this.blocks[this.blocks.length - 1]
+  }
 
-        return hash
-    }
+  generateHash(block) {
+
+      let hash = sha256(block.key)
+
+      while(!hash.startsWith("000")) {
+        block.nonce += 1
+        hash = sha256(block.key)
+        console.log(hash)
+      }
+
+      return hash
+  }
+
 }
 
 module.exports = Blockchain
--- index.js
+++ index.js
@@ -1,10 +1,33 @@
+const express = require('express')
+
 const Block = require('./block')
 const Blockchain = require('./blockchain')
 const Transaction = require('./transaction')
 
-const transaction = new Transaction('Aurelius', 'Augustus', 1337)
+const app = express()
 
-const genesisBlock = new Block()
-const blockchain = new Blockchain(genesisBlock)
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
 
-console.log(blockchain)
+app.get('/blockchain', (req, res) => {
+    
+    const transaction = new Transaction('Aurelius', 'Augustus', 1337)
+    
+    const genesisBlock = new Block()
+    const blockchain = new Blockchain(genesisBlock)
+    
+    const block = blockchain.getNextBlock([transaction])
+    blockchain.addBlock(block)
+
+    const anotherTransaction = new Transaction('Some', 'One', 7)
+    const block1 = blockchain.getNextBlock([anotherTransaction, transaction])
+    blockchain.addBlock(block1)
+
+    res.json(blockchain)
+})
+
+app.listen(3000, () => console.log('Server has started'))
+
+
+
