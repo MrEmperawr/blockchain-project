d4047c97515524897dfbea0939761983bf8aef36
Marcus <marcus.kalmar@gmail.com>
2019-11-28 13:34:03 +0100

    Update blockchain class

--- blockchain.js
+++ blockchain.js
@@ -1,3 +1,6 @@
+const sha256 = require('./node_modules/js-sha256')
+const Block = require('./block')
+
 class Blockchain {
     constructor (genesisBlock) {
         this.blocks = []
@@ -13,8 +16,32 @@ class Blockchain {
         this.blocks.push(block)
     }
 
+    getNextBlock(transactions) {
+        const block = new Block()
+
+        transactions.forEach((transaction) => {
+            block.addTransaction(transaction)
+        })
+
+        const previousBlock = this.getPreviousBlock()
+        block.index = this.blocks.length
+        block.previousHash = previousBlock.hash
+        block.hash = this.generateHash(block)
+    }
+
+    getPreviousBlock() {
+        return this.blocks[this.block.length - 1]
+    }
+
     generateHash(block) {
         const hash = sha256(block.key)
+
+        while(!hash.startsWith('00000')) {
+            block.nonce += 1
+            hash = sha256(block.key)
+            console.log(hash)
+        }
+
         return hash
     }
 }
--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const Block = require('./block')
 const Blockchain = require('./blockchain')
+const Transaction = require('./transaction')
+
+const transaction = new Transaction('Aurelius', 'Augustus', 1337)
+
+const genesisBlock = new Block()
+const blockchain = new Blockchain(genesisBlock)
+
+console.log(blockchain)
